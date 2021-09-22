f3d12400dde26a4a1c6aed07242f3275825edc61
t-nakamura <1am1goodtime@gmail.com>
2021-09-22 15:39:58 +0900

    Chapter04

---
+++ Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String name = "田中圭";
+		System.out.println(name);
+
+		name = "ありがとう";
+		System.out.println(name);
+
+		final int LUCKY_NUMBER = 8;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
